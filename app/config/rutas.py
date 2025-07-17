49e23377d03379304509c601091fb716d2f11235
Martin Alonso Gajardo Aylwin <m.gajardo29@Alumnos.Santotomas.cl>
2025-07-17 17:39:57 -0400

    a

--- app/config/rutas.py
+++ app/config/rutas.py
@@ -1,3 +1,7 @@
 import os
-BASE_PATH= os.path.dirname(os.path.abspath(__file__))  # apunta a app/config/
-BASE_PATH = os.path.normpath(os.path.join(BASE_PATH, ".."))  # sube a app/
+import sys
+if hasattr(sys, '_MEIPASS'):
+    BASE_PATH = sys._MEIPASS
+else:
+    BASE_PATH= os.path.dirname(os.path.abspath(__file__))  # apunta a app/config/
+    BASE_PATH = os.path.normpath(os.path.join(BASE_PATH, ".."))  # sube a app/
